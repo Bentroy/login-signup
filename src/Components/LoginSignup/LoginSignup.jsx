c5827d70b05e525bd1b189dca0dd7cfce18227ff
Bentroy <bentroy006@gmail.com>
2025-04-22 20:19:20 +0100

    finished up with basic styling i'm pulling

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -15,11 +15,11 @@ const LoginSignup = () => {
         <div className="inputs">
             <div className="input">
                 <img src={user_icon} alt="" />
-                <input type="text" />
+                <input type="text" placeholder="Name" />
             </div>
             <div className="input">
                 <img src={email_icon} alt="" />
-                <input type="email" />
+                <input type="email" placeholder="Email Id"/>
             </div>
             <div className="input">
                 <img src={password_icon} alt="" />
@@ -28,7 +28,7 @@ const LoginSignup = () => {
         </div>
 
         <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
-        
+
         <div className="submit-container">
             <div className="submit">Sign Up</div>
             <div className="submit">Login</div>
